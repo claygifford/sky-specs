4022b08809dd4983dea96c7eb10188fca31ad23d
unknown <claygifford@gmail.com>
2021-05-10 13:50:00 -0400

    removed the local host reference

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,14 +1,14 @@
-//import { Pool } from 'pg';
+import { Pool } from 'pg';
 
 class Db {
     constructor(host, port, user, database, password) {
-        // this.pool = new Pool({
-        //     host: host,
-        //     post: port,
-        //     user: user,
-        //     database: database,
-        //     password: password
-        // });
+        this.pool = new Pool({
+            host: host,
+            post: port,
+            user: user,
+            database: database,
+            password: password
+        });
     }
     private pool;
     

0747186336eb52dd53bd0c40a0764513b313464c
unknown <claygifford@gmail.com>
2021-05-15 10:31:11 -0400

    adding back the pg refernce

--- server/dist/db/index.js
+++ server/dist/db/index.js
@@ -1,15 +1,15 @@
 "use strict";
-//import { Pool } from 'pg';
 Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
 class Db {
     constructor(host, port, user, database, password) {
-        // this.pool = new Pool({
-        //     host: host,
-        //     post: port,
-        //     user: user,
-        //     database: database,
-        //     password: password
-        // });
+        this.pool = new pg_1.Pool({
+            host: host,
+            post: port,
+            user: user,
+            database: database,
+            password: password
+        });
     }
     query(text, params) {
         return this.pool.query(text, params);
